e52911e9eba34bba8ec8580930e0745cfea3f320
Javad <mail@heidarpoor.ir>
2020-06-20 23:56:56 +0430

    23:56

--- src/aliveCreature/Main.java
+++ src/aliveCreature/Main.java
@@ -31,16 +31,22 @@ public class Main {
 //>>>>>>> da119932cac0b04899b8d55622a08a1176c417e1
 
         Emberizidae z1 = new Emberizidae(1,1,1,1,1,1,"lemon",1,1);
-        z1.breathing();
-        z1.fly();
-        z1.layingEggs();
-//        z1.layingEggs();
-        z1.mating();
-        z1.migrate();
-//        z1.migrate();
-        z1.move("b");
-        z1.nutrition();
-        z1.seedEater();
+        System.out.println("yrouz ye zard pare limooee to tabiat migasht");
+        System.out.println("ye banoo ro did ye del na sad del asheghesh shod \n ba ham bishatr ashna shodan zar pareh mard barash intori avaz khoond");
         z1.signing();
+        System.out.println("ba ham hamnafas shodan intori :|");
+        z1.breathing();
+        System.out.println("baadesh parvaz kardan ba ham va mohajerat karan canada ba found tahsili :/");
+        z1.fly();
+        z1.migrate();
+        System.out.println("ba ham too kelisa mosalmoona ezdevaj kardan ))");
+        z1.mating();
+        z1.layingEggs();
+        System.out.println("ejareh khonashoon ziad boud raftan kabk");
+        z1.move("kabk");
+        System.out.println("ba ham ghaza khordan roozegar toolani");
+        z1.nutrition();
+        System.out.println("dooneh spicel ham mikhordan");
+        z1.seedEater();
     }
 }
