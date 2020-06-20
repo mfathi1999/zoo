9709e87e7ebbcba0d0bcb193ff140da5d11972c6
Javad <mail@heidarpoor.ir>
2020-06-20 21:01:17 +0430

    other override method added

--- src/aliveCreature/Emberizidae.java
+++ src/aliveCreature/Emberizidae.java
@@ -4,6 +4,46 @@ public class Emberizidae extends Passeriformes{
     private int conicalBeak;
     private int species;
 
+    @Override
+    public void fly() {
+        super.fly();
+    }
+
+    @Override
+    public void layingEggs() {
+        super.layingEggs();
+    }
+
+    @Override
+    public void migrate() {
+        super.migrate();
+    }
+
+    @Override
+    public void move() {
+        super.move();
+    }
+
+    @Override
+    public void nutrition() {
+        super.nutrition();
+    }
+
+    @Override
+    public void breathing() {
+        super.breathing();
+    }
+
+    @Override
+    public void mating() {
+        super.mating();
+    }
+
+    @Override
+    public void signing() {
+        super.signing();
+    }
+
     public void seedEater()
     {
 
--- src/aliveCreature/Passeriformes.java
+++ src/aliveCreature/Passeriformes.java
@@ -3,6 +3,41 @@ package aliveCreature;
 public class Passeriformes extends Birds {
     private int syrinx;
 
+    @Override
+    public void fly() {
+        super.fly();
+    }
+
+    @Override
+    public void layingEggs() {
+        super.layingEggs();
+    }
+
+    @Override
+    public void migrate() {
+        super.migrate();
+    }
+
+    @Override
+    public void move() {
+        super.move();
+    }
+
+    @Override
+    public void nutrition() {
+        super.nutrition();
+    }
+
+    @Override
+    public void breathing() {
+        super.breathing();
+    }
+
+    @Override
+    public void mating() {
+        super.mating();
+    }
+
     public void signing() {
 
     }
